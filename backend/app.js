3f598eba92a42cebc1dd8a3d15b1687abded0113
Navnit Anand <54511815+navnit28@users.noreply.github.com>
2021-11-06 08:52:32 +0530

    error Handler added

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 app.use(express.json())
+const errorMiddleware = require("./middleware/error");
 //Route Imports
 const product = require("./routes/productRoute");
 const user = require("./routes/userRoute");
@@ -8,5 +9,6 @@ const order = require("./routes/orderRoute");
 app.use("/api/v1",product);
 app.use("/api/v1",user);
 app.use("/api/v1",order);
-
+//middleware for Error
+app.use(errorMiddleware);
 module.exports = app;
--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,22 +1,21 @@
 const Order =require('../models/orderModel');
-
-exports.createOrder = async (req, res,next) => {
+const ErrorHandler = require('../utils/errorHandler');
+const catchAsyncError=require('../middleware/catchAsyncError');
+exports.createOrder = catchAsyncError( async (req, res,next) => {
     const order = await Order.create(req.body);
     res.status(201).json({
         success: 'true',
         order
     })
 }
-exports.getAllOrders = async (req, res,next) => {
+)
+exports.getAllOrders = catchAsyncError( async (req, res,next) => {
     const orders = await Order.find({user:req.params.id});
     if(!orders){
-        return res.status(404).json({
-            success: 'false',
-            message: 'No orders found'
-        })
+        return next(new ErrorHandler("Product Not found",404));
     }
     res.status(200).json({
         success: 'true',
         orders
     })
-}
+})
