776781f58122a0719ee24d901aa54698cee72ffc
Navnit Anand <54511815+navnit28@users.noreply.github.com>
2021-11-22 22:39:40 +0530

    filter added

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,61 +1,67 @@
-const Product =require("../models/productModel")
-const ErrorHandler = require('../utils/errorHandler');
-const catchAsyncError=require('../middleware/catchAsyncError');
+const Product = require("../models/productModel");
+const ErrorHandler = require("../utils/errorHandler");
+const catchAsyncError = require("../middleware/catchAsyncError");
 const ApiFeatures = require("../utils/apifeatures");
 //create Product admin route
-exports.createProduct = async (req,res,next) =>{
-    const product = await Product.create(req.body);
+exports.createProduct = async (req, res, next) => {
+  const product = await Product.create(req.body);
 
-    res.status(201).json({
-        success: 'true',
-        product
-    })
-}
+  res.status(201).json({
+    success: "true",
+    product,
+  });
+};
 //get all products
-exports.getAllProducts = catchAsyncError( async (req,res) =>{
-    const apiFeature= new ApiFeatures(Product.find(),req.query).search();
-    const products=await apiFeature.query;
-    res.status(200).json({
-        success: 'true',
-        products
-    })
-})
-exports.updateProduct = catchAsyncError( async (req,res,next) =>{
-    let product = await Product.findById(req.params.id)
-    if(!product){
-        return next(new ErrorHandler('Product not found',404));
-    }
-    product=await Product.findByIdAndUpdate(req.params.id,req.body,{
-        new:true,
-        runValidators:true,
-        useFindAndModify:false
-    })
-    res.status(200).json({
-        success: 'true',
-        product
-    })
-})
+exports.getAllProducts = catchAsyncError(async (req, res) => {
+  const resultPerPage = 5;
+  const productCount = await Product.countDocuments(); 
+  const apiFeature = new ApiFeatures(Product.find(), req.query)
+    .search()
+    .filter()
+    .pagination(resultPerPage);
+  const products = await apiFeature.query;
+  res.status(200).json({
+    success: "true",
+    products,
+  });
+});
+exports.updateProduct = catchAsyncError(async (req, res, next) => {
+  let product = await Product.findById(req.params.id);
+  if (!product) {
+    return next(new ErrorHandler("Product not found", 404));
+  }
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+  res.status(200).json({
+    success: "true",
+    product,
+  });
+});
 //delete Product
-exports.deleteProduct = catchAsyncError( async (req,res,next) =>{
-    let product = await Product.findById(req.params.id)
-    if(!product){
-      return next(new ErrorHandler('Product not found',404));
-    }
-    await product.remove();
-    res.status(200).json({
-        success: 'true',
-        message: 'Product deleted'
-    })
-})
+exports.deleteProduct = catchAsyncError(async (req, res, next) => {
+  let product = await Product.findById(req.params.id);
+  if (!product) {
+    return next(new ErrorHandler("Product not found", 404));
+  }
+  await product.remove();
+  res.status(200).json({
+    success: "true",
+    message: "Product deleted",
+  });
+});
 //get single product
-exports.getSingleProduct = catchAsyncError( async (req,res,next) =>{
-    const product= await Product.findById(req.params.id);
-    if(!product){
-        return  next(new ErrorHandler('Product not found',404));
-    }
-    res.status(200).json({
-        success: 'true',
-        product
-    })
-}
-)
+exports.getSingleProduct = catchAsyncError(async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+  const productCount = await Product.countDocuments();
+  if (!product) {
+    return next(new ErrorHandler("Product not found", 404));
+  }
+  res.status(200).json({
+    success: "true",
+    product,
+    productCount
+  });
+});
--- backend/utils/apifeatures.js
+++ backend/utils/apifeatures.js
@@ -3,15 +3,35 @@ class ApiFeaturesService {
     this.query = query;
     this.queryString = queryString;
   }
-  search(){
-      const keyword=this.queryString.keyword?{
-          name:{
-              $regex:this.queryString.keyword,
-              $options:'i',
-          }
-      }:{}
-        this.query=this.query.find({...keyword});
-        return this;
+  search() {
+    const keyword = this.queryString.keyword
+      ? {
+          name: {
+            $regex: this.queryString.keyword,
+            $options: "i",
+          },
+        }
+      : {};
+    this.query = this.query.find({ ...keyword });
+    return this;
+  }
+  filter() {
+    const queryCopy = { ...this.queryString };
+    //Removing some field from query
+    // console.log(queryCopy);
+    const removeFields = ["page", "limit", "keyword"];
+    removeFields.forEach((key) => delete queryCopy[key]);
+    let queryStr = JSON.stringify(queryCopy);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+    this.query = this.query.find(JSON.parse(queryStr));
+    return this;
+  }
+  pagination(resultsPerPage) {
+    const currentPage = Number(this.queryString.page) || 1;
+    const skip = resultsPerPage * (currentPage - 1);
+    this.query = this.query.limit(resultsPerPage).skip(skip);
+    return this;
   }
 }
 module.exports = ApiFeaturesService;
